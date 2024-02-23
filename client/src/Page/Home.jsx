6c7749b5aafb4541c9052a4943e731d67d52e03e
rjkush17 <rishabhkushwahrj@gmail.com>
2024-02-23 09:16:33 +0530

    home page desgin completed

--- client/src/Page/Home.jsx
+++ client/src/Page/Home.jsx
@@ -6,53 +6,76 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FaPinterest } from "react-icons/fa";
 import { Parallax } from "react-parallax";
 import home_single from "../assets/home/home_single.jpg";
-import product from "../assets/home/product.jpg"
+import product from "../assets/home/product.jpg";
+import blogImage from "../assets/home/blog.jpg";
 
 function Home() {
-
-
   const listingData = [
     {
-      productname : "Running shoes for men",
-      cost : 99,
-      img : product
+      productname: "Running shoes for men",
+      cost: 99,
+      img: product,
     },
     {
-      productname : "Running shoes for men",
-      cost : 99,
-      img : product
+      productname: "Running shoes for men",
+      cost: 99,
+      img: product,
     },
     {
-      productname : "Running shoes for men",
-      cost : 99,
-      img : product
+      productname: "Running shoes for men",
+      cost: 99,
+      img: product,
     },
     {
-      productname : "Running shoes for men",
-      cost : 99,
-      img : product
+      productname: "Running shoes for men",
+      cost: 99,
+      img: product,
     },
     {
-      productname : "Running shoes for men",
-      cost : 99,
-      img : product
+      productname: "Running shoes for men",
+      cost: 99,
+      img: product,
     },
     {
-      productname : "Running shoes for men",
-      cost : 99,
-      img : product
+      productname: "Running shoes for men",
+      cost: 99,
+      img: product,
     },
     {
-      productname : "Running shoes for men",
-      cost : 99,
-      img : product
+      productname: "Running shoes for men",
+      cost: 99,
+      img: product,
     },
     {
-      productname : "Running shoes for men",
-      cost : 99,
-      img : product
-    }
-  ]
+      productname: "Running shoes for men",
+      cost: 99,
+      img: product,
+    },
+  ];
+
+  const blog = [
+    {
+      blogName: "Running Shoes for men",
+      date: "April 24 2023",
+      description:
+        "This is a wider card with supporting text below as a natural lead-in to additional content.",
+      img: blogImage,
+    },
+    {
+      blogName: "Running Shoes for men",
+      date: "April 24 2023",
+      description:
+        "This is a wider card with supporting text below as a natural lead-in to additional content.",
+      img: blogImage,
+    },
+    {
+      blogName: "Running Shoes for men",
+      date: "April 24 2023",
+      description:
+        "This is a wider card with supporting text below as a natural lead-in to additional content.",
+      img: blogImage,
+    },
+  ];
 
   return (
     <>
@@ -108,11 +131,10 @@ function Home() {
         </div>
       </section>
 
- 
       {/* Product shoes section */}
       <section className="text-center w-11/12 mt-6 mobile:mt-auto mx-auto">
         <p className="text-3xl mobile:text-5xl font-extrabold text-center">Trending Products</p>
-        <p className="mt-6 tracking-widest text-lg mobil:text-2xl font-light text-center mb-4"> <button className="mx-2">Featured</button> |<button className="mx-2">Latest</button> | <button className="mx-2">Best Seller</button> | <button className="mx-2">Sale Products</button></p>
+        <p className="my-6 tracking-widest text-lg mobile:text-2xl font-light text-center mb-4"> <button className="mx-2">Featured</button> |<button className="mx-2">Latest</button> | <button className="mx-2">Best Seller</button> | <button className="mx-2">Sale Products</button></p>
         <div className="grid gap-8 grid-cols-1 mobile:grid-cols-2 grid:grid-cols-4 tablet:grid-cols-3">
           {listingData.map((val, ind)=>(
             <div key={ind} className="mx-auto">
@@ -125,7 +147,7 @@ function Home() {
         </div>
       </section>
 
-           {/* sales section */}
+      {/* sales section */}
       <section className="text-white text-center">
         <Parallax
           strength={600}
@@ -143,6 +165,36 @@ function Home() {
         </Parallax>
       </section>
 
+      {/* Product react section */}
+      <section className="text-center w-10/12 mt-6 mobile:mt-auto mx-auto">
+        <p className="my-20 text-3xl mobile:text-5xl font-extrabold text-center">
+        Latest Post
+        </p>
+        <div className="grid gap-8 grid-cols-1 mobile:grid-cols-2 tablet:grid-cols-3">
+          {blog.map((val, ind) => (
+            <div key={ind} className="mx-auto">
+              <div className="min-w-24 min-h-24 overflow-hidden">
+                <img
+                  src={val.img}
+                  className="w-full h-full object-cover hover:scale-150 transition duration-1000	 ease-in-out delay-150"
+                  alt={val.productname}
+                />
+              </div>
+              <div className="flex flex-col text-start justify-between my-4 text-lg leading-8 w-10/12">
+
+              <span className="text-gray-500 font-medium">{val.date}</span>
+
+                <h1 className="mt-4 leading-8 text-4xl">{val.blogName}</h1>{" "}
+               
+                <br />
+                <p className="text-gray-500 text-xl">{val.description}</p>
+
+                <button className="py-2 mt-6 px-4 border border-black w-fit text-black hover:bg-black hover:text-white rounded-full">Read More</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
