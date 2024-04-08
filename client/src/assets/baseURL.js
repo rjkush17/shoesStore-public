c687aec8d1d1b01ff5fac4c52fcc26135b3ea913
rjkush17 <rishabhkushwahrj@gmail.com>
2024-04-08 21:45:54 +0530

    new api added

--- client/src/assets/baseURL.js
+++ client/src/assets/baseURL.js
@@ -1,3 +1,6 @@
-export const baseURL = "https://shoesstore-backend.onrender.com/api/";
+//export const baseURL = "https://shoesstore-backend.onrender.com/";
 
-//export const baseURL = "http://localhost:5900/api/";
+//export const baseURL = "http://localhost:5900/";
+
+export const baseURL = "https://stylish-shoes-mern.vercel.app/";
+
